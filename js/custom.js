325275194be4a3c9819d818fa39fe71a65a33433
alancastrom5 <79289644+alancastrom5@users.noreply.github.com>
2024-04-15 17:31:18 -0300

    up

--- js/custom.js
+++ js/custom.js
@@ -50,16 +50,44 @@ $('.owl-filter-bar').on('click', '.item', function (e) {
 
     e.preventDefault();
 })
-/** google_map js **/
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+
 
 // nice select
 $(document).ready(function () {
     $('select').niceSelect();
 });
+
+
+// calculos de orcamento
+// salvar em uma var e enviar junto ao link do whatsapp
+
+// Selecione o campo de entrada
+const numberInput = document.querySelector('#exampleInputNumber');
+
+// Adicione um ouvinte de evento para capturar o valor quando o usuário inseri-lo
+numberInput.addEventListener('input', function() {
+    // Captura o valor do campo de entrada
+    const valorCEP = numberInput.value;
+    
+    // Salva o valor em localStorage junto com o timestamp atual
+    const expirationTime = Date.now() + 5 * 60 * 1000; // 5 minutos em milissegundos
+    const valorASalvar = (parseFloat(valorCEP) > 0) ? '100,00' : valorCEP;
+    localStorage.setItem('valorCEP', JSON.stringify({ valor: valorASalvar, expirationTime: expirationTime }));
+});
+
+// Recupera o valor armazenado em localStorage quando necessário
+const valorSalvoString = localStorage.getItem('valorCEP');
+if (valorSalvoString) {
+    const { valor, expirationTime } = JSON.parse(valorSalvoString);
+    const currentTime = Date.now();
+    if (currentTime < expirationTime) {
+        // O valor ainda está dentro do prazo de validade
+        console.log('Valor salvo:', valor);
+        const valorFormatado = (valor === '100,00') ? valor : `R$: ${valor}`;
+        document.querySelector('.form-label_1').innerHTML += ` ${valorFormatado}`;
+    } else {
+        // O valor expirou, então remova-o do localStorage
+        localStorage.removeItem('valorCEP');
+        console.log('O valor do CEP expirou e foi removido.');
+    }
+}
